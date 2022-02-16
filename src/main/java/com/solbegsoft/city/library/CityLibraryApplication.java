78b7161e1afd36aba452ab798ef369335e47472e
AliaksandrAnashkevich <alex.anashkevich@gmail.com>
2022-02-16 18:15:04 +0300

    initial commit

---
+++ src/main/java/com/solbegsoft/city/library/CityLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.solbegsoft.city.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solbegsoft/city/library/CityLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solbegsoft.city.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
